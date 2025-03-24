3866192203b733b2b40fa92c04772cfb8f71bcfd
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-03-24 16:07:36 -0300

    Tabela com todos os pacientes

--- src/pages/gestao_pacientes/index.tsx
+++ src/pages/gestao_pacientes/index.tsx
@@ -1,24 +1,30 @@
-import  { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   Container,
-  Form,
   Button,
-  ListGroup,
+  Table,
 } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { GetPatients } from '../../api/routesPacientes';
+import { InterfacePatientRegistration } from '../../types';
 import './PatientManagement.css';
-import { Navigate, useNavigate } from 'react-router-dom';
 
 export function PatientManagement() {
-  const [searchName, setSearchName] = useState('');
-  const [searchCpf, setSearchCpf] = useState('');
-  const [searchResults, setSearchResults] = useState<string[]>([]);
-
-  const handleSearch = () => {
-    const results = [`Resultado para ${searchName} e ${searchCpf}`];
-    setSearchResults(results);
-  };
-
+  const [pacientes, setPacientes] = useState<InterfacePatientRegistration[]>([]);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await GetPatients();
+        console.log('data', data);
+        setPacientes(data);
+      } catch (error) {
+        console.error('Error fetching patients:', error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <Container className="mt-4">
@@ -26,57 +32,50 @@ export function PatientManagement() {
         <h1>Gestão de Pacientes</h1>
       </header>
       <main>
-        <section id="search-patient" className="mb-4">
-          <h2>Pesquisar Paciente</h2>
-          <Form id="search-form">
-            <Form.Group>
-              <Form.Label htmlFor="search-name">Nome:</Form.Label>
-              <Form.Control
-                type="text"
-                id="search-name"
-                placeholder="Digite o nome do paciente..."
-                value={searchName}
-                onChange={(e) => setSearchName(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label htmlFor="search-cpf">CPF:</Form.Label>
-              <Form.Control
-                type="text"
-                id="search-cpf"
-                placeholder="Digite o CPF..."
-                value={searchCpf}
-                onChange={(e) => setSearchCpf(e.target.value)}
-              />
-            </Form.Group>
-            <Button variant="primary" id="search-button" onClick={handleSearch}>
-              Pesquisar
-            </Button>
-          </Form>
-          <ListGroup id="search-results" className="mt-3">
-            {searchResults.map((result, index) => (
-              <ListGroup.Item key={index}>{result}</ListGroup.Item>
+        <Table responsive bordered>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Nome</th>
+              <th>Email</th>
+              <th>Telefone</th>
+              <th>Idade</th>
+              <th>Altura</th>
+              <th>Peso</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pacientes.map((paciente) => (
+              <tr key={paciente.id}>
+                <td>{paciente.id}</td>
+                <td>{paciente.name}</td>
+                <td>{paciente.email}</td>
+                <td>{paciente.phone}</td>
+                <td>{paciente.age}</td>
+                <td>{paciente.height}</td>
+                <td>{paciente.weight}</td>
+              </tr>
             ))}
-          </ListGroup>
-        </section>
-        <section id="add-patient" className="mb-4 text-center">
-          <Button
-            variant="success"
-            id="add-patient-btn"
-            onClick={() => navigate('/cadastro-paciente')}
-          >
-            ➕ Cadastrar Paciente
-          </Button>
-        </section>
-        <section className="text-center">
-          <Button
-            variant="info"
-            onClick={() => navigate('/avaliacao-sarcopenia')}
-          >
-            Avaliação de Sarcopenia
-          </Button>
-        </section>
+          </tbody>
+        </Table>
       </main>
+      <section id="add-patient" className="mb-4 text-center">
+        <Button
+          variant="success"
+          id="add-patient-btn"
+          onClick={() => navigate('/cadastro-paciente')}
+        >
+          ➕ Cadastrar Paciente
+        </Button>
+      </section>
+      <section className="text-center">
+        <Button
+          variant="info"
+          onClick={() => navigate('/avaliacao-sarcopenia')}
+        >
+          Avaliação de Sarcopenia
+        </Button>
+      </section>
     </Container>
   );
 }
